bdea7a5dff32e74b07e4db17b04ed94f5da435f1
Sebastian Paul Muzalewski <sebmuzalew@gmail.com>
2022-12-08 19:32:39 -0500

    Lots of new changes added

--- components/ViewLatest.js
+++ components/ViewLatest.js
@@ -1,15 +1,17 @@
 /* @Page: 3 | ViewLatest
- * Component is used to render the viewLastest news page of the application.
- * 
- * 
+ * Breifing: Component is used to render the viewLastest news page of the application.
+ * API02: RefreshAPI | Status: Started | Purpose: To render a random api based on a button being pressed
+ * Animation04: Sequence animation | Status : Incomplete | Purpose: NA
  */
 
 import React from 'react'
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 // api key = 4a5e392a47c042e6b534cffbfe4fa64d
 //api link https://financialtimesmikilior1v1.p.rapidapi.com/getAspectsList
 
-function ViewLatest() {
+import FinancialNews from './FinancialNews';
+
+function ViewLatest({navigation}) {
 
   var url =  'https://newsapi.org/v2/everything?' +
   'sortBy=popularity&' +
@@ -27,6 +29,7 @@ function ViewLatest() {
   return ( 
     <View>
       <Text>ViewLatest page to render latest news.</Text>
+      <Button title="Next Page" onPress={() => navigation.navigate("FinancialNews")}></Button>
     </View>
   )
 }
