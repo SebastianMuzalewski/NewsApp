db3de91f87b17d65fd68d27c2dd546cc4785b5da
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-12-10 13:57:02 -0500

    Leave a comment page added, this page contains our one instance of an alert, and allows for user interaction

--- components/Comment.js
+++ components/Comment.js
@@ -1,12 +1,73 @@
+/*
+This page will demonstrate our ability to produce alerts 
+when a user enters in their comment and presses a button. 
+
+
+*/
+
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput , Button} from 'react-native';
+// our necessary react native components are all imported in the line above. 
+
+import{Alert} from 'react-native';
+// here we have our alert component to handle alerts when user presses a button.
 
 function Comment() {
+ 
   return (
-    <View>
-      <Text>LEAVE YOUR COMMENT HERE</Text>
+    // make a view which contains the text input and the button, and make an alert when the button is pressed to let the user know they are about to submit a comment
+
+    <View style={styles.container}>
+      <Text style={styles.text}>Tell us about  the interesting article you saw on the view latest news page.  </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your comment here"
+        placeholderTextColor="black"
+      />
+      <Button
+        title="Submit"
+        onPress={() => Alert.alert('Your comment has been submitted')}
+      />
+
+      
+
     </View>
+
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+  
+    padding: 40,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 300,
+    height: 50,
+    backgroundColor: '#fff',
+    margin: 10,
+
+    padding: 8,
+    color: 'black',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  
+});
+
+
 
 export default Comment;
--- components/Home.js
+++ components/Home.js
@@ -1,26 +1,90 @@
 /* @Page: 1 | Home Page
  * Breifing: Component is used to render home page of the application.
- * 
- * 
+
+  The home page will cotain 
+
  */
 
-import Search from './Search';
-//import ViewLatest from './components/ViewLatest';
+import FinancialNews from './FinancialNews';
+import ViewLatest from './ViewLatest';
+
+import { NavigationContainer } from '@react-navigation/native';
+
 
 import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Modal} from 'react-native';
+import { Alert } from 'react-native';
+
+
 
 function Home({ navigation }) {
   return (
-    <View>
+    // make a view in a modal to display home page, make a button to navigate to the viewLatest page and make a button to navigate to the financial news page and put them in an alert to let the user know they are being navigated to a new page
+    <View style={styles.container}>
 
 
-      <Text>Home page to render Application description.</Text>
-      
-      <Button title="SEARCH PAGE" onPress={() => navigation.navigate("Search")}></Button>
-      <Button title="VIEW LATEST" onPress={() => navigation.navigate("ViewLatest")}></Button>
+      <Modal>
+   
+   
+      <Text style={styles.text}>Welcome to the News App</Text>
+   
+   
+      <Button
+
+        title="View Latest News"
+        onPress={() => navigation.navigate('ViewLatest')}
+        styles={styles.button}
+      ></Button>
+   
+   
+      <Button
+        title="View Financial News"
+        onPress={() => navigation.navigate('FinancialNews')}
+        styles={styles.button}
+      ></Button>
+   
+
+
+      </Modal>
+   
+   
+   
     </View>
+    
+      
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+
+    padding: 40,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 300,
+    height: 50,
+    backgroundColor: '#fff',
+    margin: 10,
+
+    padding: 8,
+    color: 'black',
+    borderRadius: 14,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+})
+
 
 export default Home
